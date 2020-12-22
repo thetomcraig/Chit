ca22bc0b8909ae24f02471a0f204ca6ec6cca3f4
Tom Craig <thetomcraig@icloud.com>
2020-12-21 19:26:23 -0800

    Update to OS-based archiving with SHA256

--- make_archive.sh
+++ make_archive.sh
@@ -1,6 +1,8 @@
 version="${1}"
 
-archive_name="chit.${version}.tar.gz"
+archive_name="chit_macos_10.15.tar.gz"
 
 tar -zcvf "${archive_name}" chit.sh build iterm
 mv "${archive_name}" archive
+
+echo $(shasum -a 256 archive/"${archive_name}" | awk '{print $1}')
