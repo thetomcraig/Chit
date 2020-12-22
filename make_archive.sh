9328da8ec6b7fa7eba7fd39fdb4e9448a49b2cbc
Tom Craig <thetomcraig@icloud.com>
2020-12-21 20:02:48 -0800

    Add theme_definitions to tar

--- make_archive.sh
+++ make_archive.sh
@@ -1,6 +1,6 @@
 archive_name="chit_macos_10.15.tar.gz"
 
-tar -zcvf "${archive_name}" chit.sh build iterm
+tar -zcvf "${archive_name}" chit.sh build iterm theme_definitions
 mv "${archive_name}" archive
 
 echo $(shasum -a 256 archive/"${archive_name}" | awk '{print $1}')
