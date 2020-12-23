2d35be756460442763ebfc3ad613017930bbbbad
Tom Craig <thetomcraig@icloud.com>
2020-12-22 21:12:05 -0800

    v7

--- make_archive.sh
+++ make_archive.sh
@@ -1,7 +1,7 @@
 version="${1}"
 archive_name="chit.${version}.tar.gz"
 
-tar -zcvf "${archive_name}" chit.sh build iterm theme_definitions
+tar -zcvf "${archive_name}" chit.sh build copy_themes iterm example_theme_definitions
 mv "${archive_name}" archive
 
 echo $(shasum -a 256 archive/"${archive_name}" | awk '{print $1}')
