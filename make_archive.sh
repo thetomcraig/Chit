514364d41c0a77c160f316124a832e8d34331273
Tom Craig <thetomcraig@icloud.com>
2020-12-23 09:58:43 -0800

    move hash to the end

--- make_archive.sh
+++ make_archive.sh
@@ -4,8 +4,8 @@ archive_name="chit.${version}.tar.gz"
 tar -zcvf "${archive_name}" chit.sh build iterm example_theme_definitions
 mv "${archive_name}" archive
 
-echo $(shasum -a 256 archive/"${archive_name}" | awk '{print $1}')
-
 git add archive
 git commit -m "Add version ${version} archive"
 git push
+
+echo $(shasum -a 256 archive/"${archive_name}" | awk '{print $1}')
